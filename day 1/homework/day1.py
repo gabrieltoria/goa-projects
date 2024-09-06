c97db3da3dfffbc81607b7367a9313330a3ff7ba
gabrieltoria <iagurgenidze1979@gmail.com>
2024-09-06 13:49:26 +0400

    commit

---
+++ day 1/homework/day1.py
@@ -0,0 +1,53 @@
+name="gabi"
+surname = "toria"
+
+
+
+
+
+print(name)
+
+
+name="gabi"
+age = 22
+height = 184.5
+
+
+knows_programing = True
+is_ugly = False
+
+isUgly = False
+
+
+print(name +" " +surname)
+
+
+
+
+print(type(age))
+
+
+
+
+
+print(name + age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
