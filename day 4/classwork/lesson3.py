a735340e3b8efba6f4d48b2cd2f396d21e8c8990
gabrieltoria <iagurgenidze1979@gmail.com>
2024-10-01 16:34:16 +0400

    commit

---
+++ day 4/classwork/lesson3.py
@@ -0,0 +1,3 @@
+print (24 * 60 * 60)
+buy = 100
+item = pen
---
+++ day 4/homework/lesson4.py
@@ -0,0 +1,12 @@
+str1 = "10"
+int1 = 23
+float1 = 20.5
+print(type(str1))
+print(type(int1))
+print(type(float1))
+str2 = int(str1)
+int2= str(int1)
+float2 = str(float1)
+print(type(str2))
+print(type(int2))
+print(type(float2))
