ca5e860811a8c66f6a449faaa33c4c296adf5b20
gabrieltoria <iagurgenidze1979@gmail.com>
2024-10-09 16:26:20 +0400

    commit

---
+++ day 5/homework/lesson5.py
@@ -0,0 +1,34 @@
+# ;1food1 = "pankaces"
+# .num = 12
+# flo = 12.0.5
+# name = "98
+# surname = 00
+# mom = 123....
+# dad = 123332,.
+# queen =112/
+# kinG = 2345563../,
+# LIO/NJ = 8871//.,
+
+food = "pankaces"
+num = 12
+flo = 12.5
+name = "98"
+surname = 10
+mom = 123
+dad = 123332
+queen = 112
+king = 2345563
+lionj = 8871
+learn = "Goa "
+animal = "Best"
+print(learn + animal)
+aura = 35
+print(35 * 8)
+print(35 + 6)
+print(35 - 19)
+print(35 / 10)
+name = "Anna"
+score = 96
+
+print (name)
+print (score)
