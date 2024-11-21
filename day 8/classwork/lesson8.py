70a5f62a3598f237bc23c9a1392f1e497e3b6c2a
gabrieltoria <iagurgenidze1979@gmail.com>
2024-11-21 16:15:52 +0400

    commit

---
+++ day 8/classwork/lesson8.py
@@ -0,0 +1,4 @@
+math = 4 > 9
+lesson = 5 < 9
+print (math and lesson )
+print (math or lesson)
---
+++ day 9/homework/lesson9.py
@@ -0,0 +1,6 @@
+print (6 > 5)
+print (2 > 1)
+print (1 < 2)
+print (1 > 0 and 2 > 1 )      
+print (2 < 7 and 7 < 9)
+print (3 = 4)
