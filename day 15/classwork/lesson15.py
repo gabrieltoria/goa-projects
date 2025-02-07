77649f6d6f58b86386e51ab60cd3618e524e8a25
gabrieltoria <iagurgenidze1979@gmail.com>
2025-02-07 13:26:07 +0400

    commit

---
+++ day 15/classwork/lesson15.py
@@ -0,0 +1,22 @@
+age = 16
+ 
+is_student = yes
+
+if age <= 16
+
+if is_student
+
+print (20% discount)
+
+
+else : 
+
+      print(10% discount)
+
+
+
+else : 
+
+
+    print (regural price)
+                
